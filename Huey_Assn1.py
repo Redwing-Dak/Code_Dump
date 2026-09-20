52d998e32007cb5500a0050c9fbedf0594ccb723
Zane Huey <zhuey.307@gmail.com>
2026-09-19 20:11:37 -0400

    Add files via upload

---
+++ Huey_Assn1.py
@@ -0,0 +1,14 @@
+#Import pandas
+import numpy as np
+
+#Make a list
+aList = [1, 2, 3, 4, 5,]
+
+#Convert to numpy array
+arr = np.array(aList)
+
+#Add 1 to all values
+arr += 1
+
+#Print new values
+print(arr)
